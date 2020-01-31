73a4343f94fff36ad4f16e1281ba46a32b45c425
lab302-15 <lab302-15@labeng.local>
2020-01-31 15:19:27 +0700

    lab10

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+	string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    int output = rand() % 9;
+    cout << "Please Enter 3 times to reveal your future." << endl;
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << grade[output] << " in this 261102";
+    return 0;
+}
